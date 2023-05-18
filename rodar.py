12151e3579d88af9dd6ee2ff9e59b837b497ffd8
Pedro Tomaz Alves <pedro.tomaz@gmail.com>
2023-05-17 21:58:37 -0400

    run 2

--- rodar.py
+++ rodar.py
@@ -1,5 +1,11 @@
-from main import Comportamento
+from main import Jogo, Jogador, Comportamento, SIMULACOES
 
 print('teste')
 print(Comportamento.ALEATORIO)
 
+jogo = Jogo(num_simulations=SIMULACOES)
+jogo.add_jogador(Jogador(comportamento=Comportamento.IMPULSIVO))
+jogo.add_jogador(Jogador(comportamento=Comportamento.EXIGENTE))
+jogo.add_jogador(Jogador(comportamento=Comportamento.CAUTELOSO))
+jogo.add_jogador(Jogador(comportamento=Comportamento.ALEATORIO))
+jogo.simulate()
