875a37334a10b204d9d81a4e06cfe16822d57320
Pedro Tomaz Alves <pedro.tomaz@gmail.com>
2023-05-17 22:00:40 -0400

    classes

--- main.py
+++ main.py
@@ -11,3 +11,20 @@ class Comportamento(Enum):
     EXIGENTE = 2
     CAUTELOSO = 3
     ALEATORIO = 4
+
+
+class Jogador:
+    def __init__(self, comportamento):
+        self.comportamento = comportamento
+        self.saldo = SALDO_INICIAL 
+        self.propriedades = []
+        self.vitorias = 0
+
+
+class Jogo:
+    def __init__(self, num_simulations):
+        self.num_simulations = num_simulations
+        self.jogadores = []
+
+    def add_jogador(self, jogador):
+        self.jogadores.append(jogador)
